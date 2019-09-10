d97d681a5776f3a3ae5832ff86ba3ba38c20ea3f
nk-coding <niklask.coding@gmail.com>
2019-09-10 18:27:44 +0200

    updated UI components

---
+++ core/src/com/nkcoding/spacegame/ShipBuilderScreen.java
@@ -0,0 +1,69 @@
+package com.nkcoding.spacegame;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Stack;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.nkcoding.ui.CodeEditor;
+import com.nkcoding.ui.ShipDesigner;
+
+public class ShipBuilderScreen implements Screen {
+
+    //fields for ui elements
+    //table that contains all the other controls
+    private Table rootTable;
+
+    //CodeEditor for the file with all methods
+    private CodeEditor codeEditor;
+
+    //ScrollPane for the componentsStack
+    private ScrollPane componentsScrollPane;
+
+    //Stack for the possible components
+    private Stack componentsStack;
+
+    //ScrollPane for the propertiesStack
+    private ScrollPane propertiesScrollPane;
+
+    //Stack for the external properties for the selected Component
+    private Stack propertiesStack;
+
+    //the main Designer for the Ship
+    private ShipDesigner shipDesigner;
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/com/nkcoding/ui/ShipDesigner.java
@@ -0,0 +1,7 @@
+package com.nkcoding.ui;
+
+import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
+
+public class ShipDesigner extends WidgetGroup {
+
+}
