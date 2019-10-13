73de4fe355ea13e385117b512d74ea1a83bd5fea
nk-coding <niklask.coding@gmail.com>
2019-10-13 15:37:17 +0200

    partial CannonBullet implementation

--- core/src/com/nkcoding/spacegame/components/Cannon.java
+++ core/src/com/nkcoding/spacegame/components/Cannon.java
@@ -1,10 +1,11 @@
 package com.nkcoding.spacegame.components;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.EdgeShape;
-import com.badlogic.gdx.physics.box2d.Shape;
+import com.badlogic.gdx.physics.box2d.*;
+import com.nkcoding.spacegame.Asset;
 import com.nkcoding.spacegame.SpaceSimulation;
 import com.nkcoding.spacegame.spaceship.*;
 
@@ -26,7 +27,12 @@ public class Cannon extends Component {
         super.act(delta);
         lastFired += delta;
         if (lastFired > 1f && isShootingProperty.get()) {
-
+            lastFired = 0;
+            CannonBullet bullet = new CannonBullet(getShip().getSpaceSimulation(),
+                    localToWorld(new Vector2(0.5f * ShipDef.UNIT_SIZE, 2f * ShipDef.UNIT_SIZE)),
+                    getShip().getRotation() + getComponentDef().getRotation() * 90 * MathUtils.degreesToRadians,
+                    0.1f, 3);
+            getShip().getSpaceSimulation().addSimulated(bullet);
         }
     }
 
@@ -42,14 +48,23 @@ public class Cannon extends Component {
         //the amount of time this bullet exists
         private float movingSince = 0f;
 
-        //the Shape of this bullet
-        private EdgeShape shape;
+        //the texture
+        private Texture texture;
 
-        protected CannonBullet(SpaceSimulation spaceSimulation, Vector2 pos1, Vector2 pos2, float velocity) {
+        //the length
+        private float length;
+
+        protected CannonBullet(SpaceSimulation spaceSimulation, Vector2 pos, float angle, float length, float velocity) {
             super(spaceSimulation, BodyDef.BodyType.KinematicBody);
             final Body body = getBody();
             body.setBullet(true);
             EdgeShape edgeShape = new EdgeShape();
+            edgeShape.set(new Vector2(0,0), new Vector2(0, length));
+            body.createFixture(edgeShape, 0f);
+            body.setTransform(pos, angle);
+            body.setLinearVelocity(new Vector2(0, velocity).rotateRad(angle));
+            this.texture = getSpaceSimulation().getAssetManager().getTexture(Asset.Bullet);
+            this.length = length;
         }
 
         @Override
@@ -60,6 +75,20 @@ public class Cannon extends Component {
             if (movingSince > 10f) {
                 getSpaceSimulation().removeSimulated(this);
             }
+        }
+
+        @Override
+        public void draw(Batch batch) {
+            Vector2 drawPos = getBody().getPosition();
+            batch.draw(this.texture,
+                    drawPos.x, drawPos.y,
+                    0, 0,
+                    0.02f, length,
+                    1,1,
+                    getBody().getAngle() * MathUtils.radiansToDegrees,
+                    0,0,
+                    this.texture.getWidth(), this.texture.getHeight(),
+                    false, false);
         }
     }
 }
