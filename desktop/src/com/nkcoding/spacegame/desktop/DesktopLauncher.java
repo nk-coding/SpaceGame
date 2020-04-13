5616e50d9c24a3013dee6fdca0eeb41a5b0f37b7
nk-coding <53957498+nk-coding@users.noreply.github.com>
2020-04-13 15:57:40 +0200

    fixed license bug

--- desktop/src/com/nkcoding/spacegame/desktop/DesktopLauncher.java
+++ desktop/src/com/nkcoding/spacegame/desktop/DesktopLauncher.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.nkcoding.spacegame.SpaceGame;
 
+import java.util.List;
+
 public class DesktopLauncher {
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
